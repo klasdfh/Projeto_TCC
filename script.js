7a2b3c0294b613c19240feed606ce9d29a89a68e
klasdfh <94660656+klasdfh@users.noreply.github.com>
2024-05-28 13:29:35 -0300

    Update script.js
    fixed spacing issues

--- script.js
+++ script.js
@@ -129,10 +129,10 @@ Sebo,HEFA,22.5,73.1%`;
 
     if (co2Total > emissaoCO2QAV || reducaoCO2 < 0 || reducaoReal > 100) {
       if (reducaoCO2 < 0) {
-        resultado += `<p>※ Percentual de aumento total de CO2 em comparação com Combustível de Aviação: ${negativeGrossHandler.toFixed(2)}%<br>
+        resultado += `<p>※ Percentual de aumento total de CO2 em comparação com Combustível de Aviação: ${negativeGrossHandler.toFixed(2)}%</br></br>
         ❌ O combustível em questão, com rota de produção ${rotaProducao}, não representa uma melhora em relação aos combustíveis tradicionais.</p>`;
       } else {
-        resultado += `<p>※ Percentual de aumento total de CO2 em comparação com Combustível de Aviação: ${negativeGrossHandler.toFixed(2)}%<br>
+        resultado += `<p>※ Percentual de aumento total de CO2 em comparação com Combustível de Aviação: ${negativeGrossHandler.toFixed(2)}%</br></br>
         ❌ O combustível em questão, com rota de produção ${rotaProducao}, não representa uma melhora em relação aos combustíveis tradicionais.</p>`;
       }
       
